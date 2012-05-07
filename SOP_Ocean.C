f07e8a2eba9e634453145199e0fd1a3270b25fe6
Drew Whitehouse <Drew.Whitehouse@gmail.com>
2012-05-07 19:19:16 +1000

    fixed the problem with chop toggled off

--- SOP_Ocean.C
+++ SOP_Ocean.C
@@ -127,241 +127,241 @@ static PRM_Default     jacobianToggleDefault = PRM_Default(0);
 PRM_Template
 SOP_Ocean::myTemplateList[] = 
 {
-    PRM_Template(PRM_INT,1,&resolutionName, resolutionDefaults,0,&resolutionRange,oceanChanged),
+  PRM_Template(PRM_INT,1,&resolutionName, resolutionDefaults,0,&resolutionRange,oceanChanged),
 
-    PRM_Template(PRM_XYZ,1,&gridName, gridDefaults,0,&gridRange,oceanChanged),
+  PRM_Template(PRM_XYZ,1,&gridName, gridDefaults,0,&gridRange,oceanChanged),
 
-    PRM_Template(PRM_FLT,1,&windspeedName, &windspeedDefault,0,&windspeedRange,oceanChanged),
+  PRM_Template(PRM_FLT,1,&windspeedName, &windspeedDefault,0,&windspeedRange,oceanChanged),
 
-    PRM_Template(PRM_ANGLE,1,&winddirName,&winddirDefault,0,&winddirRange,oceanChanged),
+  PRM_Template(PRM_ANGLE,1,&winddirName,&winddirDefault,0,&winddirRange,oceanChanged),
 
-    PRM_Template(PRM_FLT,1,&smallestName, &smallestDefault,0,&smallestRange,oceanChanged),
+  PRM_Template(PRM_FLT,1,&smallestName, &smallestDefault,0,&smallestRange,oceanChanged),
 
-    PRM_Template(PRM_FLT,1,&fiddleName, &fiddleDefault,0,&fiddleRange,oceanChanged),
+  PRM_Template(PRM_FLT,1,&fiddleName, &fiddleDefault,0,&fiddleRange,oceanChanged),
 
-    PRM_Template(PRM_INT,1,&seedName, 0,0,0,oceanChanged),
+  PRM_Template(PRM_INT,1,&seedName, 0,0,0,oceanChanged),
 
-    PRM_Template(PRM_TOGGLE,1,&chopToggleName, &chopToggleDefault,0,0,oceanChanged), 
+  PRM_Template(PRM_TOGGLE,1,&chopToggleName, &chopToggleDefault,0,0,oceanChanged), 
 
-    PRM_Template(PRM_FLT,1,&chopName, &chopDefault,0,&chopRange), 
+  PRM_Template(PRM_FLT,1,&chopName, &chopDefault,0,&chopRange), 
 
-    PRM_Template(PRM_FLT,1,&dampName, &dampDefault,0,&dampRange,oceanChanged), 
+  PRM_Template(PRM_FLT,1,&dampName, &dampDefault,0,&dampRange,oceanChanged), 
 
-    PRM_Template(PRM_FLT,1,&wind_alignName, &wind_alignDefault,0,&wind_alignRange,oceanChanged), 
+  PRM_Template(PRM_FLT,1,&wind_alignName, &wind_alignDefault,0,&wind_alignRange,oceanChanged), 
 
-    PRM_Template(PRM_FLT,1,&depthName, &depthDefault,0,&depthRange,oceanChanged), 
+  PRM_Template(PRM_FLT,1,&depthName, &depthDefault,0,&depthRange,oceanChanged), 
 
-    PRM_Template(PRM_FLT_J,1,&timeName, &timeDefault,0,&timeRange,0), 
+  PRM_Template(PRM_FLT_J,1,&timeName, &timeDefault,0,&timeRange,0), 
 
-    PRM_Template(PRM_TOGGLE,1,&interpToggleName, &interpToggleDefault,0,0), 
+  PRM_Template(PRM_TOGGLE,1,&interpToggleName, &interpToggleDefault,0,0), 
 
-    PRM_Template(PRM_TOGGLE,1,&normalsToggleName, &normalsToggleDefault,0,0,oceanChanged), 
+  PRM_Template(PRM_TOGGLE,1,&normalsToggleName, &normalsToggleDefault,0,0,oceanChanged), 
 
-    PRM_Template(PRM_TOGGLE,1,&jacobianToggleName, &jacobianToggleDefault,0,0,oceanChanged), 
+  PRM_Template(PRM_TOGGLE,1,&jacobianToggleName, &jacobianToggleDefault,0,0,oceanChanged), 
 
-    PRM_Template()
+  PRM_Template()
 };
 
 OP_Node *
 SOP_Ocean::myConstructor(OP_Network *net, const char *name, OP_Operator *op)
 {
-    return new SOP_Ocean(net, name, op);
+  return new SOP_Ocean(net, name, op);
 }
 
 SOP_Ocean::SOP_Ocean(OP_Network *net, const char *name, OP_Operator *op) : SOP_Node(net, name, op)
 {
-    _ocean = 0;
-    _ocean_context = 0;
-    _ocean_scale = 1.0f;
+  _ocean = 0;
+  _ocean_context = 0;
+  _ocean_scale = 1.0f;
 
-    _ocean_needs_rebuild = true;
+  _ocean_needs_rebuild = true;
 
 }
 
 SOP_Ocean::~SOP_Ocean()
 {
-    if (_ocean)
-    {
-        delete _ocean;
-    }
-    if (_ocean_context)
-    {
-        delete _ocean_context;
-    }
+  if (_ocean)
+  {
+    delete _ocean;
+  }
+  if (_ocean_context)
+  {
+    delete _ocean_context;
+  }
 }
 
 unsigned
 SOP_Ocean::disableParms()
 {
-    unsigned changes = SOP_Node::disableParms();
+  unsigned changes = SOP_Node::disableParms();
 
-    changes += enableParm("normals_toggle",CHOP(0) ? 0 : 1);
+  changes += enableParm("normals_toggle",CHOP(0) ? 0 : 1);
 
-    return changes;
+  return changes;
 }
 
 int
 SOP_Ocean::oceanChanged(void * data, int,  float, const PRM_Template *)
 {
-    SOP_Ocean* o = reinterpret_cast<SOP_Ocean*>(data);
-    o->oceanNeedsRebuild();
-    return 1;
+  SOP_Ocean* o = reinterpret_cast<SOP_Ocean*>(data);
+  o->oceanNeedsRebuild();
+  return 1;
 }
 
 OP_ERROR
 SOP_Ocean::cookMySop(OP_Context &context)
 {
-    float now = context.getTime();
+  float now = context.getTime();
 
-    //std::cout << "cook ocean, t = " << now << std::endl;
+  //std::cout << "cook ocean, t = " << now << std::endl;
 
-    // lock inputs
-    if (lockInputs(context) >= UT_ERROR_ABORT )
+  // lock inputs
+  if (lockInputs(context) >= UT_ERROR_ABORT )
+  {
+    return error();
+  }
+
+
+  GEO_Point        *ppt;
+  UT_Interrupt    *boss;
+
+  // Check to see that there hasn't been a critical error in cooking the SOP.
+  if (error() < UT_ERROR_ABORT)
+  {
+    boss = UTgetInterrupt();
+
+    // Start the interrupt server
+    boss->opStart("Updating Ocean");
+
+    duplicatePointSource(0,context);
+
+    int   gridres  = 1 << int(GRID_RES(now));
+    float stepsize = GRID_SIZE(now) / (float)gridres;
+
+    bool do_chop     = CHOP(now);
+    bool do_jacobian = JACOBIAN(now);
+    bool do_normals  = NORMALS(now) && !do_chop;
+
+    if (!_ocean || _ocean_needs_rebuild)
     {
-        return error();
+      if (_ocean)
+      {
+        delete _ocean;
+      }
+
+      if (_ocean_context)
+      {
+        delete _ocean_context;
+      }
+
+      _ocean = new drw::Ocean(gridres,gridres,stepsize,stepsize,
+                              V(0),L(0),1.0,W(0),1-DAMP(0),FOOALIGN(0),
+                              DEPTH(0),SEED(0));
+      _ocean_scale   = _ocean->get_height_normalize_factor();
+
+      _ocean_context = _ocean->new_context(true,do_chop,do_normals,do_jacobian);
+
+      _ocean_needs_rebuild = false;
+      //             std::cout << "######### SOP, rebuilt ocean, norm_factor = " << _ocean_scale 
+      //                       << " chop = " << do_chop 
+      //                       << " norm = " << do_normals
+      //                       << " jacobian = " << do_jacobian
+      //                       << std::endl;
     }
 
+    float chop_amount = CHOPAMOUNT(now);
 
-    GEO_Point		*ppt;
-    UT_Interrupt	*boss;
+    // sum up the waves at this timestep
+    _ocean->update(TIME(now),*_ocean_context,true,do_chop,do_normals,do_jacobian,
+                   _ocean_scale * SCALE(now),chop_amount);
 
-    // Check to see that there hasn't been a critical error in cooking the SOP.
-    if (error() < UT_ERROR_ABORT)
+    bool linterp = ! INTERP(now);
+
+
+    // get our attribute indices
+    GA_RWAttributeRef normal_index;
+    GA_RWAttributeRef jminus_index;
+    GA_RWAttributeRef eminus_index;
+
+    if (do_normals)
     {
-        boss = UTgetInterrupt();
+      normal_index = gdp->addNormalAttribute(GEO_POINT_DICT);
+    }
+    if (do_jacobian)
+    {
+      // jminus_index = gdp->addPointAttrib("mineigval",sizeof(float),GB_ATTRIB_FLOAT,0);
+      // eminus_index = gdp->addPointAttrib("mineigvec",sizeof(UT_Vector3),GB_ATTRIB_VECTOR,0);
+      jminus_index = gdp->addTuple(GA_STORE_REAL32,GA_ATTRIB_POINT,"mineigval",1,GA_Defaults(0));
+      eminus_index = gdp->addFloatTuple(GA_ATTRIB_POINT,"mineigvec",1,GA_Defaults(0));
+    }
 
-        // Start the interrupt server
-        boss->opStart("Updating Ocean");
-
-        duplicatePointSource(0,context);
-
-        int   gridres  = 1 << int(GRID_RES(now));
-        float stepsize = GRID_SIZE(now) / (float)gridres;
-
-        bool do_chop     = CHOP(now);
-        bool do_jacobian = JACOBIAN(now);
-        bool do_normals  = NORMALS(now) && !do_chop;
-
-        if (!_ocean || _ocean_needs_rebuild)
-        {
-            if (_ocean)
-            {
-                delete _ocean;
-            }
-
-            if (_ocean_context)
-            {
-                delete _ocean_context;
-            }
-
-            _ocean = new drw::Ocean(gridres,gridres,stepsize,stepsize,
-                                    V(0),L(0),1.0,W(0),1-DAMP(0),FOOALIGN(0),
-                                    DEPTH(0),SEED(0));
-            _ocean_scale   = _ocean->get_height_normalize_factor();
-
-            _ocean_context = _ocean->new_context(true,do_chop,do_normals,do_jacobian);
-
-            _ocean_needs_rebuild = false;
-//             std::cout << "######### SOP, rebuilt ocean, norm_factor = " << _ocean_scale 
-//                       << " chop = " << do_chop 
-//                       << " norm = " << do_normals
-//                       << " jacobian = " << do_jacobian
-//                       << std::endl;
-        }
-
-        float chop_amount = CHOPAMOUNT(now);
-
-        // sum up the waves at this timestep
-        _ocean->update(TIME(now),*_ocean_context,true,do_chop,do_normals,do_jacobian,
-                       _ocean_scale * SCALE(now),chop_amount);
-
-        bool linterp = ! INTERP(now);
-
-
-        // get our attribute indices
-        GA_RWAttributeRef normal_index;
-        GA_RWAttributeRef jminus_index;
-        GA_RWAttributeRef eminus_index;
-
-        if (do_normals)
-        {
-            normal_index = gdp->addNormalAttribute(GEO_POINT_DICT);
-        }
-        if (do_jacobian)
-        {
-            // jminus_index = gdp->addPointAttrib("mineigval",sizeof(float),GB_ATTRIB_FLOAT,0);
-            // eminus_index = gdp->addPointAttrib("mineigvec",sizeof(UT_Vector3),GB_ATTRIB_VECTOR,0);
-            jminus_index = gdp->addTuple(GA_STORE_REAL32,GA_ATTRIB_POINT,"mineigval",1,GA_Defaults(0));
-            eminus_index = gdp->addFloatTuple(GA_ATTRIB_POINT,"mineigvec",1,GA_Defaults(0));
-        }
-
-        // this is not that fast, can it be done quicker ???
-        GA_FOR_ALL_GPOINTS(gdp, ppt)
-            {
-                UT_Vector4 p = ppt->getPos();
+    // this is not that fast, can it be done quicker ???
+    GA_FOR_ALL_GPOINTS(gdp, ppt)
+    {
+      UT_Vector4 p = ppt->getPos();
 
                 
-                if (linterp)
-                {
-                    _ocean_context->eval_xz(p(0),p(2));
-                }
-                else
-                {
-                    _ocean_context->eval2_xz(p(0),p(2));
-                }
+      if (linterp)
+      {
+        _ocean_context->eval_xz(p(0),p(2));
+      }
+      else
+      {
+        _ocean_context->eval2_xz(p(0),p(2));
+      }
 
-                if (do_chop) 
-                {
-                    p.assign( p(0) + _ocean_context->disp[0],
-                              p(1) + _ocean_context->disp[1],
-                              p(2) + _ocean_context->disp[2] );
-                }
-                else
-                {
-                    // ppt->getPos()(1) += _ocean_context->disp[1];
-                	UT_Vector4 tmp_p = ppt->getPos();
-                	tmp_p(1) += _ocean_context->disp[1];
-                	ppt->setPos(tmp_p);
-                }
+      if (do_chop) 
+      {
+        p.assign( p(0) + _ocean_context->disp[0],
+                  p(1) + _ocean_context->disp[1],
+                  p(2) + _ocean_context->disp[2] );
+      }
+      else
+      {
+        p.assign(p(0), p(1)+_ocean_context->disp[1], p(2));
+      }
 
-                if (do_normals)
-                {
-                	/*
-					  UT_Vector3* normal = (UT_Vector3*) ppt->castAttribData<UT_Vector3>(normal_index);
-					  normal->assign(_ocean_context->normal[0],
-					                 _ocean_context->normal[1],
-					                 _ocean_context->normal[2]);
-					  normal->normalize();
-                    */
-                	ppt->getValue<UT_Vector3>(normal_index).assign(_ocean_context->normal[0],
-																   _ocean_context->normal[1],
-																   _ocean_context->normal[2]);
-                	ppt->getValue<UT_Vector3>(normal_index).normalize();
-                }
+      if (do_normals)
+      {
+        /*
+          
+          UT_Vector3* normal = (UT_Vector3*) ppt->castAttribData<UT_Vector3>(normal_index);
+          normal->assign(_ocean_context->normal[0],
+          _ocean_context->normal[1],
+          _ocean_context->normal[2]);
+          normal->normalize();
+        */
+        ppt->getValue<UT_Vector3>(normal_index).assign(_ocean_context->normal[0],
+                                                       _ocean_context->normal[1],
+                                                       _ocean_context->normal[2]);
+        ppt->getValue<UT_Vector3>(normal_index).normalize();
+      }
 
-                if (do_jacobian)
-                {/*
-                    float *js = (float*)ppt->castAttribData<float>(jminus_index);
-                    *js = _ocean_context->Jminus;
-                    UT_Vector3* eminus = (UT_Vector3*)ppt->castAttribData<UT_Vector3>(eminus_index);
-                    eminus->assign(_ocean_context->Eminus[0],0,_ocean_context->Eminus[1]);
-                    */
-                    ppt->setValue<float>(jminus_index,_ocean_context->Jminus);
-                    ppt->getValue<UT_Vector3>(eminus_index).assign(_ocean_context->Eminus[0],0,_ocean_context->Eminus[1]);
-                }
-				ppt->setPos(p);
-            }
-
-
-        // Tell the interrupt server that we've completed
-        boss->opEnd();
+      if (do_jacobian)
+      {/*
+         float *js = (float*)ppt->castAttribData<float>(jminus_index);
+         *js = _ocean_context->Jminus;
+         UT_Vector3* eminus = (UT_Vector3*)ppt->castAttribData<UT_Vector3>(eminus_index);
+         eminus->assign(_ocean_context->Eminus[0],0,_ocean_context->Eminus[1]);
+       */
+        ppt->setValue<float>(jminus_index,_ocean_context->Jminus);
+        ppt->getValue<UT_Vector3>(eminus_index).assign(_ocean_context->Eminus[0],0,_ocean_context->Eminus[1]);
+      }
+      ppt->setPos(p);
     }
 
-    unlockInputs();
 
-    OP_Node::flags().setTimeDep(true);
+    gdp->notifyCache(GU_CACHE_ALL);
 
-    return error();
+    // Tell the interrupt server that we've completed
+    boss->opEnd();
+  }
+
+  unlockInputs();
+
+  OP_Node::flags().setTimeDep(true);
+
+  return error();
 }
 
 //
@@ -371,14 +371,14 @@ SOP_Ocean::cookMySop(OP_Context &context)
 class OP_OceanOperator : public OP_Operator 
 {
 
-public:
+ public:
 
-    OP_OceanOperator();
-    virtual ~OP_OceanOperator();
+  OP_OceanOperator();
+  virtual ~OP_OceanOperator();
 
-/*     virtual void	getOperatorHelp(const char *pathname, UT_String &str, */
-/*                                     int level, bool *is_html_ptr, */
-/*                                     bool try_other_levels) const; */
+  /*     virtual void    getOperatorHelp(const char *pathname, UT_String &str, */
+  /*                                     int level, bool *is_html_ptr, */
+  /*                                     bool try_other_levels) const; */
 
 };
 
@@ -390,12 +390,12 @@ OP_OceanOperator::OP_OceanOperator(): OP_Operator("ocean",
                                                   1,
                                                   0)        
 {
-    // nothing
+  // nothing
 }
 
 OP_OceanOperator::~OP_OceanOperator() 
 {
-    // nothing
+  // nothing
 }
 
 // todo: make this more sylish, possibly copy the houdini help style ?
@@ -404,24 +404,24 @@ void SOP_Ocean::getHelpText (UT_String &help,int, bool *)
 
   help  = "<h3>Ocean SOP\n\n</h3>";  
 
-   help  += "<h4>Description:</h4>";
+  help  += "<h4>Description:</h4>";
 
   help  += "" 
-      "<p>"
-      "This SOP is an implementation of the Ocean model described in the Siggraph course "
-      "<a href=\"http://www.finelightvisualtechnology.com/docs/coursenotes2004.pdf\">Simulating Ocean Surfaces</a>,"
-      " by <a href=\"http://www.finelightvisualtechnology.com/index.php\">Jerry Tessendorf</a>."
-      "It generates a wave surface by randomly "
-      "generating a set of waves with a spectrum approximated by the heuristic equation described by "
-      "Phillips. The waves are then rapidly summed together using a fast fourier transform(FFT)."
-      "</p>";
+           "<p>"
+           "This SOP is an implementation of the Ocean model described in the Siggraph course "
+           "<a href=\"http://www.finelightvisualtechnology.com/docs/coursenotes2004.pdf\">Simulating Ocean Surfaces</a>,"
+           " by <a href=\"http://www.finelightvisualtechnology.com/index.php\">Jerry Tessendorf</a>."
+           "It generates a wave surface by randomly "
+           "generating a set of waves with a spectrum approximated by the heuristic equation described by "
+           "Phillips. The waves are then rapidly summed together using a fast fourier transform(FFT)."
+           "</p>";
   help  += "" 
-      "<p>"
-      "The SOP is a modifier that additively adjusts the incoming points, with wave height acting in the y direction and "
-      "chop displacement in the xz plane."
-      " The modifier simply wraps the computation grid onto the input grid. The periodic nature of FFT's guarantees"
-      " the result tiles correctly."
-      "</p>";
+           "<p>"
+           "The SOP is a modifier that additively adjusts the incoming points, with wave height acting in the y direction and "
+           "chop displacement in the xz plane."
+           " The modifier simply wraps the computation grid onto the input grid. The periodic nature of FFT's guarantees"
+           " the result tiles correctly."
+           "</p>";
 
   help  += "<h4>Parameters:</h4>";
   
@@ -446,21 +446,21 @@ void
 newSopOperator(OP_OperatorTable *table)
 {
 #if 0
-    // If we want to use the "standard" operator class, we can do this here.
-    // However, the OP_OceanOperator has the help built in (and doesn't require
-    // an external file).
-    table->addOperator(
-        new OP_Operator("Ocean",			// Internal name
-        "Ocean",			// UI name
-        SOP_Ocean::myConstructor,	// How to build the SOP
-        SOP_Ocean::myTemplateList,	// My parameters
-        0,				// Min # of sources
-        0,				// Max # of sources
-        SOP_Ocean::myVariables,	// Local variables
-        OP_FLAG_GENERATOR)		// Flag it as generator
-        );
+  // If we want to use the "standard" operator class, we can do this here.
+  // However, the OP_OceanOperator has the help built in (and doesn't require
+  // an external file).
+  table->addOperator(
+      new OP_Operator("Ocean",            // Internal name
+                      "Ocean",            // UI name
+                      SOP_Ocean::myConstructor,    // How to build the SOP
+                      SOP_Ocean::myTemplateList,    // My parameters
+                      0,                // Min # of sources
+                      0,                // Max # of sources
+                      SOP_Ocean::myVariables,    // Local variables
+                      OP_FLAG_GENERATOR)        // Flag it as generator
+                     );
 #else
-    table->addOperator(new OP_OceanOperator());
+  table->addOperator(new OP_OceanOperator());
 #endif
 }
 
